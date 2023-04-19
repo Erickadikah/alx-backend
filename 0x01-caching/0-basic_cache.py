336617f5fc7d1a428ca1f1fe294d373533cec791
erickadikah <erickadikah2030@gmail.com>
2023-04-19 08:09:34 +0300

    task

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Basic Caching
+"""
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """caching system that inherits from BaseCaching.
+    """
+
+    def put(self, key, item):
+        """adding a key/value pair to the cache
+        """
+        if key or item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieveing a value from the cache
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
+
+
