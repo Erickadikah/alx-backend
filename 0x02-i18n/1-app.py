0a0eb1ca868b7626cb070364d62b554822e99cbe
erickadikah <erickadikah2030@gmail.com>
2023-04-25 19:14:58 +0300

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""
+"""Basic Babel setup
 """
 from flask import request
 from flask import Flask, render_template
@@ -7,11 +7,15 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
 @app.route('/', strict_slashes=False)
 def language_conf() -> str:
-    """
+    """Config class that has a LANGUAGES
+        class attribute equal to ["en", "fr"].
     """
     return render_template('1-index.html')
+
 
 class Config(object):
     """Babel instance to configure available
@@ -21,6 +25,7 @@ class Config(object):
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 if __name__ == ('__main__'):
     app.run()
